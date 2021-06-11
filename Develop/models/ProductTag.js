d8c47c2875de91369d8e557226e42b5910bead16
Lewis Hill <lewis.james.hill@outlook.com>
2021-06-11 10:23:33 +1000

    added product tag models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,20 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        productId: {
+            type: DataTypes.INTEGER,
+            references: {
+                key: "id",
+                modelName: "product",
+            },
+        },
+        tagId: {
+            type: DataTypes.INTEGER,
+            references: {
+                key: "id",
+                model: "tag",
+            },
+        },
+        // define columns
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "productTag",
+    }
 );
 
 module.exports = ProductTag;
