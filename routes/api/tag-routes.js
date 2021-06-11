14a15dd2d1186a327df87f129a6805b13bc26acd
Lewis Hill <lewis.james.hill@outlook.com>
2021-06-11 16:31:50 +1000

    tag routes completed - CRUD implemented

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,21 +15,54 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
     // find a single tag by its `id`
+    const { tagId } = req.params.id;
+    try {
+        const tag = await Tag.findByPk({ where: { id: tagId } });
+        res.status(200).json(tag);
+    } catch (err) {
+        res.status(500);
+        res.send(err);
+    }
     // be sure to include its associated Product data
 });
 
-router.post("/", (req, res) => {
-    // create a new tag
+router.post("/", async (req, res) => {
+    // create Ida new tag
+    const { tagId, tagName } = req.body;
+    try {
+        const newTag = await Tag.create({ id: tagId, tagName: tagName });
+        res.status(200).json(newTag);
+    } catch (err) {
+        res.status(500);
+    }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
+    const { tagId } = req.params;
+    const { newTagId, newTagName } = req.body;
+    try {
+        const newTag = await Tag.update(
+            { id: newTagId, tagName: newTagName },
+            { where: { id: tagId } }
+        );
+        res.json(newTag);
+    } catch (err) {
+        res.status(500);
+    }
     // update a tag's name by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
     // delete on tag by its `id` value
+    const { tagId } = req.params;
+    try {
+        const deletedTag = await Tag.destroy({ where: { id: tagId } });
+        res.status(200).json(deletedTag);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
